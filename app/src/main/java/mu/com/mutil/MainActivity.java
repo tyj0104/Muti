bf624615311c10fe0492749e2fc0faa5cf07906e
tyj0104 <254103715@qq.com>
2018-07-25 14:02:30 +0800

    上传项目

---
+++ app/src/main/java/mu/com/mutil/MainActivity.java
@@ -0,0 +1,16 @@
+package mu.com.mutil;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mu.mutil.MLMain;
+
+public class MainActivity extends MLMain {
+
+    @Override
+    public void mCreate() {
+        super.mCreate();
+        setContentView(R.layout.activity_main);
+    }
+}
